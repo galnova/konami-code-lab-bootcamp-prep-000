2e6d87cb05e11b9d660e0c1fe97dd728f0531793
Keith Jeter <kjeter2001@yahoo.com>
2018-01-18 03:51:20 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,20 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-let index = 0;
-function init() {
-  document.addEventListener('keydown', function(e) {
-  const key = parseInt(e.which || e.detail);
-    if (key === code[index]) {
-      index++;
-      if (index === code.length) {
-        window.alert('You are awesome!');
-        index = 0;
-        e.preventDefault();
-    }
-  } else {
-      index = 0;
-    }
-  console.log(e.which);
-  console.log(index);
-  console.log(key);
-});
-}
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+// let index = 0;
+// function init() {
+//   document.addEventListener('keydown', function(e) {
+//   const key = parseInt(e.which || e.detail);
+//     if (key === code[index]) {
+//       index++;
+//       if (index === code.length) {
+//         window.alert('You are awesome!');
+//         index = 0;
+//         e.preventDefault();
+//     }
+//   } else {
+//       index = 0;
+//     }
+//   console.log(e.which);
+//   console.log(index);
+//   console.log(key);
+// });
+// }
